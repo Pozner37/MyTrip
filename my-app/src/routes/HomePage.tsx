653643ce8f6e8ea8c96e4ff3cbd9f11e76d07295
Jumpmane <idopozner10@gmail.com>
2023-12-20 20:55:59 +0200

    styled search bar

--- my-app/src/routes/HomePage.tsx
+++ my-app/src/routes/HomePage.tsx
@@ -1,20 +1,23 @@
-import { Card, Stack, SxProps, TextField } from "@mui/material";
+import { Card, InputAdornment, Stack, SxProps, TextField } from "@mui/material";
 import CountriesPanel from "../components/CountriesPanel";
 import { blue } from "@mui/material/colors";
 import { useEffect, useState } from "react";
 import { BaseCountry } from "../components";
 import { getAllBaseCountries } from "../utils/countriesUtils";
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 
 const countriesPanelStyle: SxProps = {
   width: "80%",
   backgroundColor: blue[200],
   padding: "2em",
   alignSelf: "center",
-  borderRadius: 10
+  borderRadius: 10,
+  maxHeight: '40em',
+  overflowY: "auto"
 };
 
 const searchFilter: SxProps = {
-  width: "80%",
+  width: "25%",
   alignSelf: "center",
   marginBottom: 2
 };
@@ -42,9 +45,15 @@ const HomePage = () => {
     <Stack sx={{ width: "100%", paddingY:5 }}>
       <TextField sx={searchFilter}
        onChange={(e : React.ChangeEvent<HTMLInputElement>) => setSearchText(e.target.value)}
-       label='search'
-       placeholder="enter country"
-        />
+       placeholder="search a country"
+       InputProps={{
+        endAdornment: (
+          <InputAdornment position="start">
+            <TravelExploreIcon />
+          </InputAdornment>
+        ),
+      }}
+        color="info"/>
       <Card sx={countriesPanelStyle}>
         <CountriesPanel countries={filteredCountries}/>
       </Card>
