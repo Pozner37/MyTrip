d9c6395024a2930128ccc323a160188a89bc8d68
Jumpmane <idopozner10@gmail.com>
2023-12-28 22:19:22 +0200

    addpost fab prototype

--- my-app/src/routes/CountryPage.tsx
+++ my-app/src/routes/CountryPage.tsx
@@ -5,24 +5,35 @@ import { getPostsByCountry } from "../utils/postsUtils";
 import { Stack } from "@mui/material";
 import { useEffect, useState } from "react";
 import { PostType } from "../components";
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 const CountryPage = () => {
   const [posts, setPosts] = useState<PostType[]>([]);
   const { name } = useParams();
 
-  useEffect(()=> {
-    if(name){
-        getPostsByCountry(name).then( res => {
-          return setPosts(res.data)});
-      }
-  },[]);
+  useEffect(() => {
+    if (name) {
+      getPostsByCountry(name).then((res) => {
+        return setPosts(res.data);
+      });
+    }
+  }, []);
+
+  const addPost() => {
+    
+  }
 
   return (
     <>
       <CountryCard name={name} />
-      <Stack spacing={2} alignItems="center" sx={{padding: '4%'}}>
-        {posts.length && posts.map((post) => <Post key={post._id} post={post}/>)}
+      <Stack spacing={2} alignItems="center" sx={{ padding: "4%" }}>
+        {posts.length &&
+          posts.map((post) => <Post key={post._id} post={post} />)}
       </Stack>
+      <Fab color="primary" sx={{position: 'absolute', left: '1%', top: '92%'}} onClick={addPost()}>
+        <AddIcon />
+      </Fab>
     </>
   );
 };
