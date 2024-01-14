b44a9199df923065b6fa9944c6e38029d60123a3
Omri Alster <omrialster@gmail.com>
2024-01-14 14:04:20 -0800

    add google login

--- my-app/src/components/AuthModal.tsx
+++ my-app/src/components/AuthModal.tsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import { Button, Modal, Box, Typography, Stack } from '@mui/material';
 import Login from './Login';
 import SignUp from './SignUp';
-import { GoogleLogin } from '@react-oauth/google';
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode'
 import { useDispatch, useSelector } from 'react-redux';
-import { UserState, setShowAuthModal } from '../redux/reducers/UserReducer';
+import { UserState, setShowAuthModal, setUser } from '../redux/reducers/UserReducer';
+import { googleLogin } from '../utils/authUtils';
 
 const AuthModal = () => {
   const appProps = useSelector((state: UserState) => state);
@@ -14,6 +15,19 @@ const AuthModal = () => {
   const dispatch = useDispatch();
 
   const onClose = () => dispatch(setShowAuthModal(false))
+
+  const handleGoogleLogin = async (res : CredentialResponse) => {
+    if(res.credential)
+    {
+        console.log(jwtDecode(res.credential))
+        await googleLogin(res.credential).then(res => {
+            if (res.status === 200) {
+                onClose();
+                dispatch(setUser(res.data))
+            }
+        })
+    }
+  }
 
   return (
     <Modal open={appProps.showAuthModal} onClose={onClose}>
@@ -38,7 +52,7 @@ const AuthModal = () => {
          : <SignUp moveToLogin={()=>setAlreadyUser(true)} setErrorLine={setErrorLine}/>}
         <Stack display='flex' useFlexGap sx={{flexDirection:'row', paddingTop: 2}}>
         <GoogleLogin
-            onSuccess={res=> res.credential && console.log(jwtDecode(res.credential))}
+            onSuccess={handleGoogleLogin}
             onError={() => {
                 console.log('Login Failed');
             }}
--- my-app/src/utils/authUtils.ts
+++ my-app/src/utils/authUtils.ts
@@ -12,3 +12,5 @@ export const getUser = async () =>
     
 export const logout = async () => 
     await axios.post<{}, AxiosResponse<{}>>('http://localhost:3000/auth/logout', undefined, {withCredentials : true});
+export const googleLogin = async (token : string) => 
+    await axios.post<{token : string}, AxiosResponse<BasicUserDto>>('http://localhost:3000/auth/google', {token : token});
